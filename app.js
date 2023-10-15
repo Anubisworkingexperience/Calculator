ae5ea6f5a1fb266710c0debdbb7c2bd85abe1e05
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2023-10-15 18:55:26 +0300

    Add basic structure and styles

---
+++ app.js
@@ -0,0 +1,12 @@
+const add = ((a, b) => a + b);
+const subtract = ((a, b) => a - b);
+const multiply = ((a, b) => a * b);
+const divide = ((a, b ) => a / b);
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, operator, secondNumber) {
+    
+}
