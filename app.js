47eb4ab7a32152ed894961e3ad7b2c3ef166c710
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2023-10-18 00:29:37 +0300

    Style changes and output

--- app.js
+++ app.js
@@ -8,5 +8,49 @@ let secondNumber;
 let operator;
 
 function operate(firstNumber, operator, secondNumber) {
-    
+    if (operator == '+') {
+        add(firstNumber, secondNumber);
+    }
+    else if (operator == '-') {
+        subtract(firstNumber, secondNumber);
+    }
+    else if (operator == '*') {
+        multiply(firstNumber, secondNumber);
+    }
+    else {
+        divide(firstNumber, secondNumber);
+    }
 }
+
+// flashing output cursor
+const output = document.querySelector('.output');
+// setInterval(function() {output.textContent = ''}, 500);
+// setInterval(function() {output.textContent = '|'}, 1000);
+
+
+const five = document.querySelector('.five');
+
+function outputWrite(button) {
+    button.addEventListener('click', () => {
+        output.textContent += button.textContent;
+    });
+};
+
+const buttons = document.querySelectorAll('button');
+
+const equals = document.querySelector('.equals');
+const del = document.querySelector('.del');
+const clear = document.querySelector('.clear');
+const sign = document.querySelector('.sign');
+const percent = document.querySelector('.percent');
+
+for (button of buttons) {
+    if ((button != equals) && (button != del) && (button != clear) &&
+        (button != sign) && (button != percent)) {
+    outputWrite(button);
+    }
+}
+
+
+
+
